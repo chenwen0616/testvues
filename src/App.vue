e446c0189bece9cb7c3a6709be4ca60b5d01c63a
chenwen <951867067@qq.com>
2021-11-22 08:27:58 +0800

    frist commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <div class="todo-container">
+        <div class="todo-wrap">
+          <HeaderTop :addTodo="addTodo"></HeaderTop>
+          <UseList :todos="todos"></UseList>
+          <UseFooter></UseFooter>
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderTop from './components/HeaderTop.vue'
+import UseFooter from './components/UseFooter.vue'
+import UseList from './components/UseList.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderTop,
+    UseList,
+    UseFooter,
+  },
+  data(){
+    return {
+      todos:[
+        {id:'001',name:'吃饭',done:true},
+        {id:'002',name:'睡觉',done:false},
+        {id:'003',name:'唱歌',done:false},
+        {id:'004',name:'跳舞',done:false},
+      ],
+    }
+  },
+  methods:{
+    addTodo(x){
+      console.log('我收到了数据：',x)
+      this.todos.unshift(x);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  /* base */
+  body{
+    background: #FFF;
+  }
+  .btn{
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+  }
+  .btn-danger{
+    color: #fff;
+    background: #da4f49;
+    border: 1px solid #bd362f;
+  }
+  .btn-danger:hover{
+    color: #fff;
+    background: #bd362f;
+  }
+  .btn:focus{
+    outline: none;
+  }
+  .todo-container{
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap{
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
 </style>
